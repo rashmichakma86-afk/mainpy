e3d73b4c78b64039c2aeec3154e511bd66790a5a
rashmichakma86-afk <rashmichakma86@gmail.com>
2026-01-01 21:48:10 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("Name:", name)
+print("data type of name is", type(name))
+print("age", age)
+print("data type of age is", type(age))
+print("is_student", is_student)
+print("data type of is_student is", type(is_student))
+print("weight", weight)
+print("data type of weight is", type(weight))
+
+print("After type casting")
+age = str(age)
+print("data type of age is", type(age))
+weight = int(weight)
+print("data type of weight is", type(age) )
